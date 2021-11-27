cb20376fa8db64109be4884755ac54b3069ed4b9
NarutoBratishka <katorabian@gmail.com>
2021-11-28 01:18:29 +0700

    change: init clear code hierarchy

--- app/src/main/java/com/katorabian/composition/presentation/MainActivity.kt
+++ app/src/main/java/com/katorabian/composition/presentation/MainActivity.kt
@@ -1,9 +1,10 @@
- package com.katorabian.composition
+ package com.katorabian.composition.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.katorabian.composition.R
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
