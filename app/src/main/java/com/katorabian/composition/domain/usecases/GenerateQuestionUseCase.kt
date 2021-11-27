ff304bfe5f1911c4523be47116dd27dfcc3ac72c
NarutoBratishka <katorabian@gmail.com>
2021-11-28 02:01:53 +0700

    add: useCases realization

--- app/src/main/java/com/katorabian/composition/domain/usecases/GenerateQuestionUseCase.kt
+++ app/src/main/java/com/katorabian/composition/domain/usecases/GenerateQuestionUseCase.kt
@@ -1,4 +1,17 @@
 package com.katorabian.composition.domain.usecases
 
-class GenerateQuestionUseCase {
+import com.katorabian.composition.domain.entity.Question
+import com.katorabian.composition.domain.repository.GameRepository
+
+class GenerateQuestionUseCase(
+    private val repository: GameRepository
+) {
+
+    operator fun invoke(maxSumValue: Int): Question {
+        return repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
+    }
+
+    private companion object {
+        private const val COUNT_OF_OPTIONS = 6
+    }
 }
--- app/src/main/java/com/katorabian/composition/domain/usecases/GetGameSettingsUseCase.kt
+++ app/src/main/java/com/katorabian/composition/domain/usecases/GetGameSettingsUseCase.kt
@@ -1,4 +1,14 @@
 package com.katorabian.composition.domain.usecases
 
-class GetGameSettingsUseCase {
+import com.katorabian.composition.domain.entity.GameSettings
+import com.katorabian.composition.domain.entity.Level
+import com.katorabian.composition.domain.repository.GameRepository
+
+class GetGameSettingsUseCase(
+    private val repository: GameRepository
+) {
+
+    operator fun invoke(level: Level): GameSettings {
+        return repository.getGameSettings(level)
+    }
 }
